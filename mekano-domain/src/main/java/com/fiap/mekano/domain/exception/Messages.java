d2cbb8d77fd18256ea886602029345abb763d84e
victor-souza-dev <victor.souza2210@gmail.com>
2026-06-21 18:18:59 -0300

    refactor: simplify messages.properties location to classpath root
    - Move messages.properties from resources/com/fiap/mekano/domain/exception/
      to src/main/resources/ (eliminates 5 levels of nesting)
    - Update Messages.java BASE_NAME to flat resource name
    - Remove generate-code from quarkus-maven-plugin execution (runs via
      extension auto-registration, avoids MapStruct broken bytecode)

--- mekano-domain/src/main/java/com/fiap/mekano/domain/exception/Messages.java
+++ mekano-domain/src/main/java/com/fiap/mekano/domain/exception/Messages.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public final class Messages {
 
-    private static final String BASE_NAME = "com.fiap.mekano.domain.exception.messages";
+    private static final String BASE_NAME = "messages";
     private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BASE_NAME);
 
     private Messages() {}
