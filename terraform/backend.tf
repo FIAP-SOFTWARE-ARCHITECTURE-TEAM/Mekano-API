465410c3c4f5982bd7a76e2d966d0ea831b8ebbc
elimafe <eliaspsm2@gmail.com>
2026-08-24 23:54:56 -0300

    terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,7 @@
 terraform {
   backend "s3" {
-    key     = "mekano/infra/terraform.tfstate"
-    encrypt = true
-
-    # Lock nativo no S3
+    key          = "mekano/infra/terraform.tfstate"
+    encrypt      = true
     use_lockfile = true
-
-    # Mantido temporariamente por requisito acadêmico.
-    # Deprecated no Terraform atual.
-    dynamodb_table = "mekano-terraform-locks"
   }
 }
